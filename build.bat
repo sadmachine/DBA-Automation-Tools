422ee482d9b1e4d6502a453dcbbafe748ca84cff
sadmachine <austin.fishbaugh@gmail.com>
2022-12-12 10:30:46 -0500

    Add new build script and assets

---
+++ build.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /in ".\DBA AutoTools.ahk" /out ".\dist\DBA AutoTools.exe" /icon ".\assets\Prag Logo.ico" 
+"C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /in ".\PO_Verification.ahk" /out ".\dist\modules\PO_Verification.exe" /icon ".\assets\Prag Logo.ico" 
