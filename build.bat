856bb5fb095cf04c29b9674bac4be4bcb774f1b2
sadmachine <austin.fishbaugh@gmail.com>
2023-02-07 15:02:26 -0500

    Fix build.bat to use base file and correct .exe

--- build.bat
+++ build.bat
@@ -10,8 +10,8 @@ tasklist /fi "imagename eq PO_Verification.exe" |find ":" > nul
 if errorlevel 1 taskkill /f /im "PO_Verification.exe"
 
 :: Build both AHK files to EXEs
-"C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /in ".\DBA AutoTools.ahk" /out ".\dist\DBA AutoTools.exe" /icon ".\assets\Prag Logo.ico" 
-"C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /in ".\PO_Verification.ahk" /out ".\dist\modules\PO_Verification.exe" /icon ".\assets\Prag Logo.ico" 
+"C:\Program Files\AutoHotkey\v2\Compiler\Ahk2Exe.exe" /base "C:\Program Files\AutoHotkey\v2\v1.1.36.02\Unicode 64-bit.bin" /in ".\DBA AutoTools.ahk" /out ".\dist\DBA AutoTools.exe" /icon ".\assets\Prag Logo.ico" 
+"C:\Program Files\AutoHotkey\v2\Compiler\Ahk2Exe.exe" /base "C:\Program Files\AutoHotkey\v2\v1.1.36.02\Unicode 64-bit.bin" /in ".\PO_Verification.ahk" /out ".\dist\modules\PO_Verification.exe" /icon ".\assets\Prag Logo.ico" 
 
 :: Reset the prompt
 @PROMPT %OLDPROMPT%
