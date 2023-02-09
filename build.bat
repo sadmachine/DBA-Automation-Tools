1b830872d714d9d2e7f2bc91c436fd3adb7e6b0a
sadmachine <austin.fishbaugh@gmail.com>
2023-02-09 17:01:06 -0500

    Create settings standalone program

--- build.bat
+++ build.bat
@@ -9,6 +9,9 @@ if errorlevel 1 taskkill /f /im "DBA AutoTools.exe"
 tasklist /fi "imagename eq PO_Verification.exe" |find ":" > nul
 if errorlevel 1 taskkill /f /im "PO_Verification.exe"
 
+tasklist /fi "imagename eq Settings.exe" |find ":" > nul
+if errorlevel 1 taskkill /f /im "Settings.exe"
+
 tasklist /fi "imagename eq tmp.exe" |find ":" > nul
 if errorlevel 1 taskkill /f /im "tmp.exe"
 
@@ -16,6 +19,7 @@ if errorlevel 1 taskkill /f /im "tmp.exe"
 "C:\Program Files\AutoHotkey\v2\Compiler\Ahk2Exe.exe" /base "C:\Program Files\AutoHotkey\v2\v1.1.36.02\Unicode 64-bit.bin" /in ".\DBA AutoTools.ahk" /out ".\dist\DBA AutoTools.exe" /icon ".\assets\Prag Logo.ico" 
 "C:\Program Files\AutoHotkey\v2\Compiler\Ahk2Exe.exe" /base "C:\Program Files\AutoHotkey\v2\v1.1.36.02\Unicode 64-bit.bin" /in ".\PO_Verification.ahk" /out ".\dist\modules\PO_Verification.exe" /icon ".\assets\Prag Logo.ico" 
 "C:\Program Files\AutoHotkey\v2\Compiler\Ahk2Exe.exe" /base "C:\Program Files\AutoHotkey\v2\v1.1.36.02\Unicode 64-bit.bin" /in ".\tmp.ahk" /out ".\tmp.exe" /icon ".\assets\Prag Logo.ico" 
+"C:\Program Files\AutoHotkey\v2\Compiler\Ahk2Exe.exe" /base "C:\Program Files\AutoHotkey\v2\v1.1.36.02\Unicode 64-bit.bin" /in ".\Settings.ahk" /out ".\dist\Settings.exe" /icon ".\assets\Prag Logo.ico" 
 
 :: Reset the prompt
 @PROMPT %OLDPROMPT%
