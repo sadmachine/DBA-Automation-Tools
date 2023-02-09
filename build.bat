9094ac1477f23ebdf95e2be528f081f7bd8a8753
sadmachine <austin.fishbaugh@gmail.com>
2023-02-09 14:47:18 -0500

    Compile tmp.exe with build

--- build.bat
+++ build.bat
@@ -9,9 +9,13 @@ if errorlevel 1 taskkill /f /im "DBA AutoTools.exe"
 tasklist /fi "imagename eq PO_Verification.exe" |find ":" > nul
 if errorlevel 1 taskkill /f /im "PO_Verification.exe"
 
+tasklist /fi "imagename eq tmp.exe" |find ":" > nul
+if errorlevel 1 taskkill /f /im "tmp.exe"
+
 :: Build both AHK files to EXEs
 "C:\Program Files\AutoHotkey\v2\Compiler\Ahk2Exe.exe" /base "C:\Program Files\AutoHotkey\v2\v1.1.36.02\Unicode 64-bit.bin" /in ".\DBA AutoTools.ahk" /out ".\dist\DBA AutoTools.exe" /icon ".\assets\Prag Logo.ico" 
 "C:\Program Files\AutoHotkey\v2\Compiler\Ahk2Exe.exe" /base "C:\Program Files\AutoHotkey\v2\v1.1.36.02\Unicode 64-bit.bin" /in ".\PO_Verification.ahk" /out ".\dist\modules\PO_Verification.exe" /icon ".\assets\Prag Logo.ico" 
+"C:\Program Files\AutoHotkey\v2\Compiler\Ahk2Exe.exe" /base "C:\Program Files\AutoHotkey\v2\v1.1.36.02\Unicode 64-bit.bin" /in ".\tmp.ahk" /out ".\tmp.exe" /icon ".\assets\Prag Logo.ico" 
 
 :: Reset the prompt
 @PROMPT %OLDPROMPT%
