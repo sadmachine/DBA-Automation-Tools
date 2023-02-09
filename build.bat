d391f595574bf8bd47c53501ec317e7f6244ef16
sadmachine <austin.fishbaugh@gmail.com>
2023-02-09 18:58:15 -0500

    Settings icons

--- build.bat
+++ build.bat
@@ -20,7 +20,7 @@ if errorlevel 1 taskkill /f /im "tmp.exe"
 "C:\Program Files\AutoHotkey\v2\Compiler\Ahk2Exe.exe" /base "C:\Program Files\AutoHotkey\v2\v1.1.36.02\Unicode 64-bit.bin" /in ".\PO_Verification.ahk" /out ".\dist\modules\PO_Verification.exe" /icon ".\assets\Prag Logo.ico" 
 "C:\Program Files\AutoHotkey\v2\Compiler\Ahk2Exe.exe" /base "C:\Program Files\AutoHotkey\v2\v1.1.36.02\Unicode 64-bit.bin" /in ".\Installer.ahk" /out ".\dist\Installer.exe" /icon ".\assets\Installer.ico" 
 "C:\Program Files\AutoHotkey\v2\Compiler\Ahk2Exe.exe" /base "C:\Program Files\AutoHotkey\v2\v1.1.36.02\Unicode 64-bit.bin" /in ".\tmp.ahk" /out ".\tmp.exe" /icon ".\assets\Prag Logo.ico" 
-"C:\Program Files\AutoHotkey\v2\Compiler\Ahk2Exe.exe" /base "C:\Program Files\AutoHotkey\v2\v1.1.36.02\Unicode 64-bit.bin" /in ".\Settings.ahk" /out ".\dist\Settings.exe" /icon ".\assets\Installer.ico" 
+"C:\Program Files\AutoHotkey\v2\Compiler\Ahk2Exe.exe" /base "C:\Program Files\AutoHotkey\v2\v1.1.36.02\Unicode 64-bit.bin" /in ".\Settings.ahk" /out ".\dist\Settings.exe" /icon ".\assets\Settings5.ico" 
 
 :: Reset the prompt
 @PROMPT %OLDPROMPT%
