327b3e98d5da070dd77806b143182a4783d79aeb
sadmachine <github.sadmachine@gmail.com>
2023-02-12 19:31:30 -0500

    Simplify build.bat config

--- build.bat
+++ build.bat
@@ -1,5 +1,8 @@
 :: Save and clear the prompt (better clarity for echoing)
 @set OLDPROMPT=%PROMPT%
+@set COMPILER="C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe"
+@set BINFILE="C:\Program Files\AutoHotkey\Compiler\Unicode 64-bit.bin"
+echo %AHK_COMPILER_DIR%
 @PROMPT $G$S
 
 :: Kill existing processes that will affect builds
@@ -16,11 +19,11 @@ tasklist /fi "imagename eq tmp.exe" |find ":" > nul
 if errorlevel 1 taskkill /f /im "tmp.exe"
 
 :: Build both AHK files to EXEs
-"C:\Program Files\AutoHotkey\v2\Compiler\Ahk2Exe.exe" /base "C:\Program Files\AutoHotkey\v2\v1.1.36.02\Unicode 64-bit.bin" /in ".\DBA AutoTools.ahk" /out ".\dist\DBA AutoTools.exe" /icon ".\assets\Prag Logo.ico" 
-"C:\Program Files\AutoHotkey\v2\Compiler\Ahk2Exe.exe" /base "C:\Program Files\AutoHotkey\v2\v1.1.36.02\Unicode 64-bit.bin" /in ".\PO_Verification.ahk" /out ".\dist\modules\PO_Verification.exe" /icon ".\assets\Prag Logo.ico" 
-"C:\Program Files\AutoHotkey\v2\Compiler\Ahk2Exe.exe" /base "C:\Program Files\AutoHotkey\v2\v1.1.36.02\Unicode 64-bit.bin" /in ".\Installer.ahk" /out ".\dist\Installer.exe" /icon ".\assets\Installer.ico" 
-"C:\Program Files\AutoHotkey\v2\Compiler\Ahk2Exe.exe" /base "C:\Program Files\AutoHotkey\v2\v1.1.36.02\Unicode 64-bit.bin" /in ".\tmp.ahk" /out ".\tmp.exe" /icon ".\assets\Prag Logo.ico" 
-"C:\Program Files\AutoHotkey\v2\Compiler\Ahk2Exe.exe" /base "C:\Program Files\AutoHotkey\v2\v1.1.36.02\Unicode 64-bit.bin" /in ".\Settings.ahk" /out ".\dist\Settings.exe" /icon ".\assets\Settings5.ico" 
+%COMPILER% /base %BINFILE% /in ".\DBA AutoTools.ahk" /out ".\dist\DBA AutoTools.exe" /icon ".\assets\Prag Logo.ico" 
+%COMPILER% /base %BINFILE% /in ".\PO_Verification.ahk" /out ".\dist\modules\PO_Verification.exe" /icon ".\assets\Prag Logo.ico" 
+%COMPILER% /base %BINFILE% /in ".\Installer.ahk" /out ".\dist\Installer.exe" /icon ".\assets\Installer.ico" 
+%COMPILER% /base %BINFILE% /in ".\tmp.ahk" /out ".\tmp.exe" /icon ".\assets\Prag Logo.ico" 
+%COMPILER% /base %BINFILE% /in ".\Settings.ahk" /out ".\dist\Settings.exe" /icon ".\assets\Settings5.ico" 
 
 :: Reset the prompt
 @PROMPT %OLDPROMPT%
